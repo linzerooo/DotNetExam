357318ccb848c56a9b55bee8149fe6fcbde45a99
Liana <a>
2024-01-09 11:08:10 +0300

    Добавлен контроллер для получения монстра из бд.

--- DotNetExam/Controllers/MonsterController.cs
+++ DotNetExam/Controllers/MonsterController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace BusinessLogicDataBase.Controllers
-{
-    public class MonsterController : Controller
+namespace BusinessLogicDataBase.Controllersс
+{ 
+    [ApiController]
+    [Route("[controller]")]
+    public class MonsterController
     {
-        public IActionResult Index()
+        private readonly MonstersDbContext _db;
+
+        public MonsterController(MonstersDbContext db) => _db = db;
+
+        [HttpGet]
+        [Route("getMonster")]
+        public JsonResult GetRandom()
         {
-            return View();
+            var count = _db.Monsters.Count();
+            int i = 0;
+            return new JsonResult(_db.Monsters.ToList().ElementAt(i++%count));
         }
     }
 }
